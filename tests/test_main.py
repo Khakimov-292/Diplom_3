577d21f9b0b298aa5d67ad0d99ab66bd35d7e32d
Khakimov-292 <kyubi292@yandex.ru>
2025-07-21 23:16:34 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,31 +6,27 @@ from pages.main_page import MainPage
 
 class TestMainFunctionality:
     @title("Переход по клику на 'Конструктор'")
-    def test_constructor_link(self, driver, user):
-        login_page = LoginPage(driver)
-        login_page.click_to_constructor()
-
+    def test_constructor_link(self, driver):
+        page = MainPage(driver)
+        page.click_to_constructor()
         assert driver.current_url == urls.MAIN_URL
 
     @title("Переход в ленту заказов")
-    def test_orders_feed(self, driver, user):
+    def test_orders_feed(self, driver):
         page = MainPage(driver)
         page.click_to_orders_feed()
-
         assert driver.current_url == urls.FEED_URL
 
     @title("Открыть всплывающее окно ингредиентов")
     def test_ingredient_modal(self, driver):
         page = MainPage(driver)
         page.click_to_first_ingredient()
-
         assert "ingredient" in driver.current_url
 
     @title("Закрыть всплывающее окно")
     def test_close_modal(self, driver):
         page = MainPage(driver)
         page.click_to_first_ingredient()
-
         page.close_ingredient_modal()
         assert page.is_modal_closed()
 
@@ -44,7 +40,6 @@ class TestMainFunctionality:
     def test_create_order(self, driver, user):
         login_page = LoginPage(driver)
         login_page.login(user["email"], user["password"])
-
         page = MainPage(driver)
         page.add_buns_to_order()
         page.make_order()
