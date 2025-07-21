5fa1a10b37e4c51a98196db131a8d7595dc0651e
Khakimov-292 <kyubi292@yandex.ru>
2025-07-21 23:16:47 +0300

    Update test_recover.py

--- tests/test_recover.py
+++ tests/test_recover.py
@@ -1,9 +1,8 @@
 from allure import title
 from data import urls
-from locators.reset_page_locators import ResetPageLocators
+from data.constants import Constant
 from pages.login_page import LoginPage
 from pages.recover_page import RecoverPage
-from pages.reset_page import ResetPage
 from helpers import generate_email
 
 
@@ -18,20 +17,14 @@ class TestRecoverPage:
     @title("Ввод почты и клик по кнопке «Восстановить»")
     def test_request_password_recover(self, driver):
         page = RecoverPage(driver)
-
         email = generate_email()
         page.enter_email(email)
         page.click_to_recover()
-
         assert driver.current_url == urls.RESET_URL
 
     @title("Показать/скрыть пароль")
-    def test_show_password(self, driver):
-        page = ResetPage(driver)
-
+    def test_click_the_show_hidden_button_field_active(self, driver):
+        page = RecoverPage(driver)
         page.enter_password("test")
-        page.click_show_password()
-
-        pwd_input = page.find_element(ResetPageLocators.PASSWORD_INPUT_PARENT)
-
-        assert "input_status_active" in pwd_input.get_attribute("class")
+        element_class = page.click_show_password()
+        assert element_class == Constant.field_password_active
