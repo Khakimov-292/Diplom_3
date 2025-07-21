3521001aaa406febc6bf040fc596888ec6a83cc5
Khakimov-292 <kyubi292@yandex.ru>
2025-07-21 23:14:29 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,19 +1,20 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 
-TIMEOUT = 10
+from locators.main_page_locators import MainPageLocators
 
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def go_to_url(self, url):
         self.driver.get(url)
 
     def wait_for_load_element(self, locator):
-        WebDriverWait(self.driver, TIMEOUT).until(
+        self.wait.until(
             expected_conditions.visibility_of_element_located(locator)
         )
 
@@ -21,15 +22,10 @@ class BasePage:
         ActionChains(self.driver).drag_and_drop(drag, drop).perform()
 
     def wait_for_clickable_element(self, locator):
-        WebDriverWait(self.driver, TIMEOUT).until(
-            expected_conditions.element_to_be_clickable(locator)
-        )
+        self.wait.until(expected_conditions.element_to_be_clickable(locator))
 
     def find_elements(self, locator):
         return self.driver.find_elements(*locator)
-
-    def find_element(self, locator):
-        return self.driver.find_element(*locator)
 
     def click_element(self, locator):
         return self.driver.find_element(*locator).click()
@@ -40,5 +36,10 @@ class BasePage:
     def scroll_to_element(self, locator):
         return self.driver.execute_script(
             "arguments[0].scrollIntoView();",
-            self.find_element(locator)
+            self.find_elements(locator)
         )
+
+    def get_attribute_class(self, locator):
+        self.wait.until(expected_conditions.invisibility_of_element_located(MainPageLocators.OVERLAYING_ELEMENT))
+        element_class = self.find_elements(locator).get_attribute('class')
+        return element_class
