7288865849de98f84746499979d1614c3520a23b
Khakimov-292 <kyubi292@yandex.ru>
2025-07-21 23:15:10 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,9 @@
 from allure import step
 
-from locators.main_page_locators import MainPageLocators
 from pages.base_page import BasePage
 from locators.login_page_locators import LoginPageLocators
-from locators.recover_page_locators import RecoverPageLocators
 from data import urls
+
 
 class LoginPage(BasePage):
     def __init__(self, driver):
@@ -16,17 +15,11 @@ class LoginPage(BasePage):
     @step("Открыть восстановление пароля")
     def open_password_recovery(self):
         self.click_element(LoginPageLocators.RECOVER_LINK)
-        self.wait_for_load_element(RecoverPageLocators.RECOVER_BTN)
-
+        self.wait_for_load_element(LoginPageLocators.RECOVER_BUTTON)
 
     @step("Логин пользователя")
     def login(self, email, password):
         self.input_to_element(LoginPageLocators.EMAIL_INPUT, email)
         self.input_to_element(LoginPageLocators.PASSWORD_INPUT, password)
         self.click_element(LoginPageLocators.ENTER_BTN)
-        self.wait_for_clickable_element(MainPageLocators.FIRST_BUN_LINK)
-
-    @step("Перейти в конструктор")
-    def click_to_constructor(self):
-        self.click_element(MainPageLocators.CONSTRUCTOR_LINK)
-        self.wait_for_clickable_element(MainPageLocators.ROOT_FIELD)
+        self.wait_for_clickable_element(LoginPageLocators.BUN_LINK)
