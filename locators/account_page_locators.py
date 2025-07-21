5df98be7ee6375e8e183965bb02319ac9105e708
Khakimov-292 <kyubi292@yandex.ru>
2025-07-21 23:12:27 +0300

    Update account_page_locators.py

--- locators/account_page_locators.py
+++ locators/account_page_locators.py
@@ -3,7 +3,11 @@ from selenium.webdriver.common.by import By
 
 class AccountPageLocators:
     ORDERS_LINK = (By.XPATH, "//a[contains(text(), 'История заказов')]")
+    ENTER_FORM = (By.XPATH, ".//h2[text() = 'Вход']")
+    PROFILE_BUTTON = (By.XPATH, "//a[text()='Профиль']")
     LOGOUT_LINK = (By.XPATH, "//button[contains(text(), 'Выход')]")
     ORDERS_ACTIVE_LINK = (By.XPATH, "//a[@class='Account_link__2ETsJ text text_type_main-medium text_color_inactive "
                                     "Account_link_active__2opc9']")
     ALL_ORDERS_NUMBER = (By.XPATH, "//p[@class='text text_type_digits-default']")
+    ACCOUNT_LINK = (By.XPATH, ".//a[@href ='/account']")
+
