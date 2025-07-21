74e5b871c0d5c72bf88e312ca369afd795c8366a
Khakimov-292 <kyubi292@yandex.ru>
2025-07-21 23:15:40 +0300

    Update recover_page.py

--- pages/recover_page.py
+++ pages/recover_page.py
@@ -1,8 +1,8 @@
 from allure import step
 
+from helpers import generate_email
 from pages.base_page import BasePage
 from locators.recover_page_locators import RecoverPageLocators
-from locators.reset_page_locators import ResetPageLocators
 from data import urls
 
 
@@ -13,6 +13,11 @@ class RecoverPage(BasePage):
         self.go_to_url(urls.RECOVER_URL)
         self.wait_for_load_element(RecoverPageLocators.RECOVER_BTN)
 
+        email = generate_email()
+        self.input_to_element(RecoverPageLocators.EMAIL_INPUT, email)
+        self.click_element(RecoverPageLocators.RECOVER_BTN)
+        self.wait_for_load_element(RecoverPageLocators.SAVE_BTN)
+
     @step("Ввод почты")
     def enter_email(self, email):
         self.input_to_element(RecoverPageLocators.EMAIL_INPUT, email)
@@ -20,4 +25,14 @@ class RecoverPage(BasePage):
     @step("Клик на кнопку 'восстановить'")
     def click_to_recover(self):
         self.click_element(RecoverPageLocators.RECOVER_BTN)
-        self.wait_for_load_element(ResetPageLocators.SAVE_BTN)
+        self.wait_for_load_element(RecoverPageLocators.SAVE_BTN)
+
+    @step("Ввод пароля")
+    def enter_password(self, pwd):
+        self.input_to_element(RecoverPageLocators.PASSWORD_INPUT, pwd)
+
+    @step("Клик по кнопке показать/скрыть пароль делает поле активным")
+    def click_show_password(self):
+        self.click_element(RecoverPageLocators.SHOW_PASSWORD_BTN)
+        element_class = self.get_attribute_class(RecoverPageLocators.RECOVER_BTN)
+        return element_class
