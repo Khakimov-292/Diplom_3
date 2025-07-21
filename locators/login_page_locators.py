9ba8a104f424f55e592339fb8ad710dd6da529ae
Khakimov-292 <kyubi292@yandex.ru>
2025-07-21 23:12:54 +0300

    Update login_page_locators.py

--- locators/login_page_locators.py
+++ locators/login_page_locators.py
@@ -4,5 +4,7 @@ from selenium.webdriver.common.by import By
 class LoginPageLocators:
     RECOVER_LINK = (By.XPATH, "//a[@href='/forgot-password']")
     EMAIL_INPUT = (By.XPATH, "//input[@name='name']")
+    BUN_LINK = (By.XPATH, "//div[@class='BurgerIngredients_ingredients__menuContainer__Xu3Mo']/ul[1]/a[1]")
     PASSWORD_INPUT = (By.XPATH, "//input[@name='Пароль']")
+    RECOVER_BUTTON = (By.XPATH, ".//button[text() = 'Восстановить']")
     ENTER_BTN = (By.XPATH, "//button[contains(text(), 'Войти')]")
